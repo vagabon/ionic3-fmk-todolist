fcdd1f45c25007fcc7d36931fac1f1468aa39ab1
gonzague <gonzague.clement@gmail.com>
2017-11-19 01:24:50 +0100

    MySimpleList :  * AdMob : toogle Bannier Ad on demand

--- src/app-fmk/providers/ad-mob-service/ad-mob-service.ts
+++ src/app-fmk/providers/ad-mob-service/ad-mob-service.ts
@@ -10,50 +10,54 @@ import {ConfigFmkServiceProvider} from "../config-fmk-service/config-fmk-service
 @Injectable()
 export class AdMobServiceProvider {
 
+  initBaniere:boolean = false;
+
   constructor(public platform: Platform, private admobFree: AdMobFree, private configService: ConfigFmkServiceProvider) {
   }
 
-  showBannierePub() {
+  showBannierePub(show = true) {
     if (this.platform.is("cordova")) {
       this.platform.ready().then(() => {
-        const bannerConfig: AdMobFreeBannerConfig = {
-          id: this.configService.API_ADMOB_BANNIER,
-          isTesting: false,
-          autoShow: true
-        };
-        this.admobFree.banner.config(bannerConfig);
-        this.admobFree.banner.prepare()
-          .then(() => {
-            this.admobFree.banner.show();
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      });
-
-      window.addEventListener("orientationchange", (e) => {
-
-        var currentOrientation = "";
-
-        if (window.orientation == 0) {
-          currentOrientation = "portrait";
-        } else if (window.orientation == 90) {
-          currentOrientation = "landscape";
-        } else if (window.orientation == -90) {
-          currentOrientation = "landscape";
-        } else if (window.orientation == 180) {
-          currentOrientation = "portrait";
-        }
-        if (currentOrientation == "portrait") {
-          this.admobFree.banner.show();
+        if (!this.initBaniere) {
+          const bannerConfig: AdMobFreeBannerConfig = {
+            id: this.configService.API_ADMOB_BANNIER,
+            isTesting: false,
+            autoShow: false
+          };
+          this.admobFree.banner.config(bannerConfig);
+          this.admobFree.banner.prepare()
+            .then(() => {
+              this.toogleBannierePub(show);
+            })
+            .catch(error => {
+              console.error(error);
+            });
+          this.hideBanniereOnLandscape();
+          this.initBaniere = true;
         } else {
-          this.admobFree.banner.hide();
+          this.toogleBannierePub(show);
         }
-      }, true);
+      });
     }
   }
 
-  toogleBannierePub(show = true) {
+  private hideBanniereOnLandscape() {
+    window.addEventListener("orientationchange", () => {
+      let currentOrientation = "";
+      if (window.orientation == 0 || window.orientation == 180) {
+        currentOrientation = "portrait";
+      } else if (window.orientation == -90 || window.orientation == 90) {
+        currentOrientation = "landscape";
+      }
+      if (currentOrientation == "portrait") {
+        this.admobFree.banner.show();
+      } else {
+        this.admobFree.banner.hide();
+      }
+    }, true);
+  }
+
+  private toogleBannierePub(show = true) {
     if (this.platform.is("cordova")) {
       if (show) {
         this.admobFree.banner.show();
