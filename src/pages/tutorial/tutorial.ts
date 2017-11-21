253e2ebdb417e01e91827993e85e2963fd490020
gonzague <gonzague.clement@gmail.com>
2017-11-21 16:47:39 +0100

    MySimpleList :  * Translate : add language in the database

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -4,6 +4,7 @@ import {IonicPage, NavController} from 'ionic-angular';
 import {TranslateService} from '@ngx-translate/core';
 import {GoogleAnalyticsServiceProvider} from "../../app-fmk/providers/google-analytics-service/google-analytics-service";
 import {DataFmkServiceProvider} from "../../app-fmk/providers/data-fmk-service/data-fmk-service";
+import {AdMobServiceProvider} from "../../app-fmk/providers/ad-mob-service/ad-mob-service";
 
 export interface Slide {
   title: string;
@@ -21,7 +22,9 @@ export class TutorialPage {
   slides: Slide[];
   showEnd: boolean = false;
 
-  constructor(public navCtrl: NavController, translate: TranslateService, private dataService: DataFmkServiceProvider, protected gAService: GoogleAnalyticsServiceProvider) {
+  constructor(public navCtrl: NavController, translate: TranslateService, private dataService: DataFmkServiceProvider, protected gAService: GoogleAnalyticsServiceProvider,
+              protected adMobService:AdMobServiceProvider) {
+    this.adMobService.showBannierePub(false);
     this.gAService.sendPageView("TutorialPage");
     // TODO : add compte desc compte facebook + synchro données + dispo Store + site Web
     translate.get([
