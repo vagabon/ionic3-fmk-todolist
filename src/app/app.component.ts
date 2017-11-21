10a6d847abd6040cdb55de7fd3913a7d61723c94
gonzague <gonzague.clement@gmail.com>
2017-11-21 02:51:54 +0100

    MySimpleList :  * DataFmkService : load data from server at start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import {SplashScreen} from '@ionic-native/splash-screen';
 import {TranslateService} from "@ngx-translate/core";
 import {GoogleAnalyticsServiceProvider} from "../app-fmk/providers/google-analytics-service/google-analytics-service";
 import {LogServiceProvider} from "../app-fmk/providers/log-service/log-service";
-import {DataFmkServiceProvider} from "../app-fmk/providers/data-fmk-service/data-fmk-service";
 import {BaseAppComponent} from "../app-fmk/components/base-app-component/base-app-component";
 
 @Component({
@@ -14,10 +13,8 @@ import {BaseAppComponent} from "../app-fmk/components/base-app-component/base-ap
 export class MyApp extends BaseAppComponent {
 
   constructor(protected translate: TranslateService, protected config: Config, protected platform: Platform, protected statusBar: StatusBar, protected splashScreen: SplashScreen,
-              protected googleAnalyticsService: GoogleAnalyticsServiceProvider, protected loggerService:LogServiceProvider, private dataService: DataFmkServiceProvider) {
+              protected googleAnalyticsService: GoogleAnalyticsServiceProvider, protected loggerService:LogServiceProvider) {
     super(translate, config, platform, statusBar, splashScreen, googleAnalyticsService, loggerService);
-    this.dataService.load();
-    this.dataService.loadFromApiId(this.dataService.data.id);
   }
 
 }
